45da7c663977adcdd617ef5921e560f3b2c4eade
bqbnakasprv7aqe6e <90489496+bqbnakasprv7aqe6e@users.noreply.github.com>
2021-10-15 21:33:46 -0400

    feat: completed
    kinda suss

--- src/main/java/edu/brown/cs/student/main/Main.java
+++ src/main/java/edu/brown/cs/student/main/Main.java
@@ -14,6 +14,8 @@ import freemarker.template.Configuration;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import joptsimple.OptionSpec;
+import org.json.JSONException;
+import org.json.JSONObject;
 import spark.*;
 import spark.template.freemarker.FreeMarkerEngine;
 
@@ -122,6 +124,7 @@ public final class Main {
     Spark.get("/autocorrect", new AutocorrectHandler(), freeMarker);
     //TODO: create a call to Spark.post to make a post request to a url which
       // will handle getting autocorrect results for the input
+    Spark.post("/suggest", new ResultsHandler());
   }
 
   /**
@@ -159,16 +162,20 @@ public final class Main {
      */
     private static class ResultsHandler implements Route {
         @Override
-        public String handle(Request req, Response res) {
+        public String handle(Request req, Response res) throws JSONException {
+            QueryParamsMap map = req.queryMap();
+            String text = map.value("text");
+            System.out.println(text);
             //TODO: Get JSONObject from req and use it to get the value of the input you want to
             // generate suggestions for
-
+//            JSONObject json = new JSONObject(req.body());
             //TODO: use the global autocorrect instance to get the suggestions
-
+//            Set<String> suggestions = ac.suggest(json.getString("text"));
+            Set<String> results = ac.suggest(text);
             //TODO: create an immutable map using the suggestions
-
+            Map variables = ImmutableMap.of("results", results);
             //TODO: return a Json of the suggestions (HINT: use the GSON.Json())
-            return null;
+            return GSON.toJson(variables);
         }
     }
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -6,35 +6,53 @@
 //TODO: select the list where the suggestions should go, the input box where we're typing, and
 // the loading text
 //HINT: look at the hTML
-const suggestionList = _;
-const input = _;
-const loading = _;
 
-input.addEventListener("keyup", () => {
-  //TODO: empty the suggestionList (you want new suggestions each time someone types something
-  // new, hint, use .innerHTML)
-  // TODO: show the loading text (HINT: set value of loading.style.display to "block" or "")
+      const suggestionList = document.getElementById('suggestions');
+      const input = document.getElementById('autocorrect-input');
+      const loading = document.getElementById('loading');
 
-  const postParameters = {
-    //TODO: get the text inside the input box (hint: use input.value to get the value of the input field)
-    text: _
-  };
+      input.addEventListener("keyup", () => {
+        //TODO: empty the suggestionList (you want new suggestions each time someone types something
+        // new, hint, use .innerHTML)
+        // TODO: show the loading text (HINT: set value of loading.style.display to "block" or "")
+        suggestionList.innerHTML = ""
+        loading.style.display = "block"
 
-  //TODO: make a post request to the url to handle this request you set in your Main.java
-  //HINT: check out the GET, POST, and JSON section of the lab
+        const postParameters = {
+          //TODO: get the text inside the input box (hint: use input.value to get the value of the input field)
+          text: input.value
+        };
 
-  //TODO: Parse the JSON in the response object
-  //HINT: remember to get the specific field in the JSON you want to use
+        //TODO: make a post request to the url to handle this request you set in your Main.java
+        //HINT: check out the GET, POST, and JSON section of the lab
+        fetch("/suggest", {
+          method: 'post',
+          body: new URLSearchParams(postParameters)
+        })
+            //TODO: Parse the JSON in the response object
+            //HINT: remember to get the specific field in the JSON you want to use
+            .then((response) => {
+              console.log(response)
+              return response.json()
+            })
+            .then((responseObject) => {
+              console.log(responseObject)
 
-  //TODO: for each element in the set of results, append it to the suggestionList
-  //HINT: use innerHTML += to append to teh suggestions list
-  //NOTE: you should use <li> (list item) tags to wrap each element. When you do so,
-  // make sure to add the attribute 'tabindex="0"' (for example: <li tabindex="0">{your element}</li>).
-  // This makes each element selectable via screen reader.
+              //TODO: for each element in the set of results, append it to the suggestionList
+              //HINT: use innerHTML += to append to teh suggestions list
+              //NOTE: you should use <li> (list item) tags to wrap each element. When you do so,
+              // make sure to add the attribute 'tabindex="0"' (for example: <li tabindex="0">{your element}</li>).
+              // This makes each element selectable via screen reader.
+              for (let i = 0; i < responseObject.results.length; i++) {
+              //TODO: add an click handler to each of the elements you added to the suggestionList
+              // with a function which will replace whatever is in input with the suggestion that
+              // was clicked
+                suggestionList.innerHTML += `<li onclick="function (responseObject) {
+                    input.value = responseObject.innerHTML
+                }"> ${responseObject.results[i]} </li>`
+              }
+              });
 
-  //TODO: add an click handler to each of the elements you added to the suggestionList
-  // with a function which will replace whatever is in input with the suggestion that
-  // was clicked
-
-  //TODO: hide the loading text
-});
+        //TODO: hide the loading text
+        loading.style.display = "none"
+      });
